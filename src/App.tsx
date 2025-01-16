b09de6e6dc28daf471bd4f79a8975cab7a48a59c
Florence KANG <fkang@outlook.fr>
2025-01-17 00:10:38 +0100

    extraire pagination component

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,22 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import { useEffect, useState } from 'react'
 import { IPokemonList, IType } from './@types'
+import Pagination from './components/Pagination/Pagination'
 
 function App() {
   const [types, setTypes] = useState<IType[]>([])
   const [pokemonsList, setPokemonsList] = useState<IPokemonList[]>([])
   
-  const [currentUrl, setCurrentUrl] = useState(`https://pokeapi.co/api/v2/pokemon?offset=00&limit=20`)
+  const [currentUrl, setCurrentUrl] = useState(`https://pokeapi.co/api/v2/pokemon`)
   const [nextUrl, setNextUrl] = useState(currentUrl)
   const [previousUrl, setPreviousUrl] = useState(currentUrl)
   
   useEffect(() => {
     const getPokemonsList = async () => {
       const {data} = await axios.get(currentUrl)
-      setPokemonsList(data.results)
       setNextUrl(data.next)
       setPreviousUrl(data.previous)
+      setPokemonsList(data.results)
     }
     
     getPokemonsList()
@@ -50,38 +51,8 @@ function App() {
             </li>
           )}
         </ul>
-
-        <div className='container my-6'>
-          <div className="pagination is-centered">
-            <button 
-              className="pagination-previous"
-              onClick={() => {
-                setCurrentUrl(previousUrl)
-              }}
-            >
-              Previous
-            </button>
-
-            <button 
-              className="pagination-next"
-              onClick={() => {
-                setCurrentUrl(nextUrl)
-              }}
-            >
-              Next page
-            </button>
-
-            <ul className="pagination-list">
-              <li><button className="pagination-link">1</button></li>
-              <li><span className="pagination-ellipsis">&hellip;</span></li>
-              <li><button className="pagination-link">45</button></li>
-              <li><button className="pagination-link is-current">46</button></li>
-              <li><button className="pagination-link">47</button></li>
-              <li><span className="pagination-ellipsis">&hellip;</span></li>
-              <li><button className="pagination-link">86</button></li>
-            </ul>
-          </div>
-        </div>
+        
+        <Pagination setCurrentUrl={setCurrentUrl} previousUrl={previousUrl} nextUrl={nextUrl} />
       </main>
 
       <Footer />
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,43 @@
+interface PaginationProps {
+  setCurrentUrl: (currentUrl: string) => void;
+  previousUrl: string;
+  nextUrl: string
+}
+
+export default function Pagination({setCurrentUrl, previousUrl, nextUrl}: PaginationProps) {
+  return (
+    <div className='container my-6'>
+      <div className="pagination is-centered">
+        <button 
+          className="pagination-previous"
+          onClick={() => {
+            setCurrentUrl(previousUrl)
+          }}
+        >
+          Previous
+        </button>
+
+        <button 
+          className="pagination-next"
+          onClick={() => {
+            setCurrentUrl(nextUrl)
+          }}
+        >
+          Next page
+        </button>
+
+        <ul className="pagination-list">
+          <li><button className="pagination-link is-current">1</button></li>
+          <li><button className="pagination-link">2</button></li>
+          <li><button className="pagination-link">3</button></li>
+          <li><span className="pagination-link">4</span></li>
+          <li><button className="pagination-link">5</button></li>
+          <li><button className="pagination-link">6</button></li>
+          <li><button className="pagination-link">7</button></li>
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+          <li><button className="pagination-link">86</button></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
