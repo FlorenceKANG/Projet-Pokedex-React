8045a51d5f2c1a78908a955084692d06bb5e69b6
Florence KANG <fkang@outlook.fr>
2025-12-22 13:20:25 +0100

    Allow all preview hosts for Render

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
+    // @ts-expect-error Vite 6 types missing allowedHosts
+    allowedHosts: "all",
   },
 });
